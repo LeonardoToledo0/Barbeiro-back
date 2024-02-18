0740ff71a1a5960acfdb2a32be5baad58bfc3b49
Leonardo Toledo <leotoledo010@gmail.com>
2024-02-18 12:48:10 -0300

    ajuste na galeria

--- Agenda/Agenda/settings.py
+++ Agenda/Agenda/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -121,11 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-if DEBUG:
-    STATIC_URL = '/static/'
-    STATICFILES_DIRS = [
-        BASE_DIR / "static",
-    ]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
